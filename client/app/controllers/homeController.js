03e2bac6e5c572c62d0df42ffcc8bf4aa4ea1c37
impersin <closer124@gmail.com>
2017-06-14 16:47:47 -0700

    Invoked Factory.getAll to retrieve iniail data

--- client/app/controllers/homeController.js
+++ client/app/controllers/homeController.js
@@ -1,4 +1,8 @@
 angular.module('eaApp.Home', [])
 .controller('HomeController', function($scope, Factory) {
   $scope.title = 'Welcom to EA Home Page';
+
+  Factory.getAll().then(function(res) {
+    $scope.matchResults = res.data;
+  });
 });
