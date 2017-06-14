d5b3a5164865f0f6a86d84e0d60c1fee67f4ad71
impersin <closer124@gmail.com>
2017-06-14 10:34:10 -0700

    Set static path for initial page.

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-Parser');
+var path = require('path');
 var app = express();
 
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/../client'));
 
 
 app.get('/', (req, res) => {
-  res.send('hello');
+  res.sendFile(path.join(__dirname, '/../client/index.html'));
 });
+
 
 var PORT = process.env.PORT || 3000;
 
