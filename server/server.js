acb8118936619508b68be4a4a98c1d2fcef94e8b
impersin <closer124@gmail.com>
2017-06-14 16:20:45 -0700

    Created get request with end poin 'api/record'

--- server/server.js
+++ server/server.js
@@ -15,6 +15,15 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/../client/index.html'));
 });
 
+app.get('/api/record', function(req, res) {
+  var query = 'SELECT p.name "Player Name", m.location "Match Location", m.duration "Match Duration", m.win_team "Win Team" ,m.lose_team "Lose Team", m.score "Final Score" FROM players p INNER JOIN matchs m ON p.id = m.winner_id';
+  db.query(query, function(err, results, fiedls) {
+    var data = JSON.stringify(results);        
+    res.send(data);
+  });
+        
+});    
+
 
 var PORT = process.env.PORT || 3000;
 
