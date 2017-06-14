60989687070aaebc63b8c520554a15c9c5ca4df3
impersin <closer124@gmail.com>
2017-06-14 16:35:19 -0700

    Created eaApp.Service compoenent with http sservice

---
+++ client/app/service/eaAppService.js
@@ -0,0 +1,13 @@
+angular.module('eaApp.Service', [])
+  .factory('Factory', function($http) {
+    return {
+      getAll: function() {
+        return $http({
+          method: 'GET',
+          url: 'api/record'
+        }).then(function(res) {
+          console.log(res);
+        });
+      }
+    };
+  });
