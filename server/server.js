67bbf6cbea16a35e7295c9e2572c76e321757acd
impersin <closer124@gmail.com>
2017-06-14 10:16:54 -0700

    Initialized server with port 3000

--- server/server.js
+++ server/server.js
@@ -1,3 +1,16 @@
-var app = require('express');
+var express = require('express');
 var bodyParser = require('body-Parser');
+var app = express();
 
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log('server listening on:' + PORT);
+});
