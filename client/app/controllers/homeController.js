f0594b399c2f5d3e8b7fdc90cbac948e59c48141
impersin <closer124@gmail.com>
2017-06-15 02:05:51 -0700

    Invoked getAll method inside of addRecord method to fetch data, and empty input values

--- client/app/controllers/homeController.js
+++ client/app/controllers/homeController.js
@@ -26,8 +26,29 @@ angular.module('eaApp.Home', [])
 
   $scope.sendRecord = function() {
     Factory.addRecord($scope.matchInfo).then(function(res) {
-      console.log(res);
+      console.log('Successfully Added');
+      Factory.getAll().then(function(res) {
+        $scope.matchResults = res.data;
+        $scope.matchResults.forEach(function(match) {
+          var score1 = parseInt(match['Final Score'][0]);
+          var score2 = parseInt(match['Final Score'][2]);
+          if (score1 > score2) {
+            match['Final Score'] += ' WON';
+          } else {
+            match['Final Score'] += ' LOST';
+          }
+        }, this);  
+        $scope.matchInfo = {
+          playerName: '',
+          location: '',
+          duration: 0,
+          playerTeam: '',
+          opponent: '',
+          result: '',
+          score: ''
+        };
+      });
     });
   };
-  
+
 });
