7fc65849205a6a7b06e337733d40632b77a61c1f
impersin <closer124@gmail.com>
2017-06-14 18:54:31 -0700

    Created scope.matchInfo variable to assign each input data

--- client/app/controllers/homeController.js
+++ client/app/controllers/homeController.js
@@ -1,11 +1,18 @@
 angular.module('eaApp.Home', [])
 .controller('HomeController', function($scope, Factory) {
   $scope.title = 'Welcom to EA Home Page';
+  $scope.matchInfo = {
+    playerName: '',
+    location: '',
+    duration: 0,
+    playerTeam: '',
+    opponent: '',
+    result: ''
+  };
 
   Factory.getAll().then(function(res) {
     $scope.matchResults = res.data;
   });
-  
   $scope.sendRecord = function() {
     Factory.addRecord($scope.matchInfo).then(function(res) {
       console.log(res);
