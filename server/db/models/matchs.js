a6df9c38f44d134a355f3c81a80b96e3577ce69e
impersin <closer124@gmail.com>
2017-06-22 17:23:33 -0700

    Used template literals

--- server/db/models/matchs.js
+++ server/db/models/matchs.js
@@ -1,18 +1,18 @@
 var db = require('../index.js');
 
-var sql = 'CREATE TABLE IF NOT EXISTS matchs (\
-  id int(100) NOT NULL AUTO_INCREMENT,\
-  duration int(200) NOT NULL,\
-  location varchar(100) NOT NULL,\
-  win_team varchar(100) NOT NULL,\
-  lose_team varchar(100) NOT NULL,\
-  score varchar(100) NOT NULL,\
-  winner_id int(100) NOT NULL,\
-  PRIMARY KEY(ID),\
-  FOREIGN KEY (winner_id) REFERENCES players(id))';
+var sql = `CREATE TABLE IF NOT EXISTS matchs (
+  id int(100) NOT NULL AUTO_INCREMENT,
+  duration int(200) NOT NULL,
+  location varchar(100) NOT NULL,
+  win_team varchar(100) NOT NULL,
+  lose_team varchar(100) NOT NULL,
+  score varchar(100) NOT NULL,
+  winner_id int(100) NOT NULL,
+  PRIMARY KEY(ID),
+  FOREIGN KEY (winner_id) REFERENCES players(id))`;
 db.query(sql, function (err, result) {
   if (err) { throw err; }
-  console.log('matchs table is created');
+  console.log('match table is created');
 });   
 
 
