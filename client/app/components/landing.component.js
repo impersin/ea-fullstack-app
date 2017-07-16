7bcbf04848e5d9a45455dd46393d1015a3baf7ff
impersin <closer124@gmail.com>
2017-07-16 15:46:23 -0700

    Created landing.component with landing.html template

---
+++ client/app/components/landing.component.js
@@ -0,0 +1,14 @@
+appModule.component('landingComponent', {
+  templateUrl: 'app/templates/landing.html',
+  controllerAs: 'landingController',
+  controller: function($scope, $http) {
+    $scope.videoUrl = 'https://www.youtube.com/watch?v=QV7PK8AVEKA?autoplay=1';
+    $scope.title = 'https://www.youtube.com/watch?v=QV7PK8AVEKA?autoplay=1';
+    
+    $scope.sendRequest = function() {
+      $http.get('/secure-api/checkRequest').then(function(res) {
+        console.log(res);
+      });
+    };
+  }
+});
