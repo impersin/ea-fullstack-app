a315f16f0fbf0dfde1ffc26e925e12fd399fcb95
impersin <closer124@gmail.com>
2017-07-16 14:44:51 -0700

    Removed MySQL connection, Created MongoDB connection

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,26 @@
-var mysql = require('mysql');
+var mongoose = require('mongoose');
+var mongoUri = 'mongodb://localhost/eaApp';
 
-var con = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'EA'
+mongoose.connect(mongoUri);
+
+var db = mongoose.connection;
+
+var importData = require('./users.json');
+var User = require('./models/Users.js');  
+
+db.once('open', () => {
+  console.log('database connected!');
 });
 
-module.exports = con;
+module.exports = db;
+
+// var mysql = require('mysql');
+
+// var con = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'EA'
+// });
+
+// module.exports = con;
