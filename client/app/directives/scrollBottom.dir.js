3b2603cb30434293f788ad014f1750cbcb868506
impersin <closer124@gmail.com>
2017-07-21 10:51:19 -0700

    Created scrollBottom directive

---
+++ client/app/directives/scrollBottom.dir.js
@@ -0,0 +1,16 @@
+angular.module('scrollBottom', [])
+.directive('scrollBottom', function () {
+  return {
+    scope: {
+      scrollBottom: '='
+    },
+    link: function (scope, element) {
+      scope.$watchCollection('scrollBottom', function (newValue) {
+        // console.log(newValue);
+        if (newValue) {
+          $(element).scrollTop($(element)[0].scrollHeight);
+        }
+      });
+    }
+  };
+});
