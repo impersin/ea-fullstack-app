181cfa94a90554dc9717a8b3350d3b7717913409
impersin <closer124@gmail.com>
2017-07-21 10:50:59 -0700

    Added $scope.message and messages to stored message data and its objects

--- client/app/components/sidebar.component.js
+++ client/app/components/sidebar.component.js
@@ -1,11 +1,17 @@
 appModule.component('sidebarComponent', {
   templateUrl: 'app/templates/sidebar.html',
   controllerAs: 'sidebarController',
-  controller: function($scope, $rootScope, $cookies, $http, $mdDialog, Factory, $window) {
+  controller: function($scope, $rootScope, $cookies, $http, $mdDialog, Factory, $window, socket, $anchorScroll, $location) {
 
     $scope.header = 'This is sidebar component!! yay';
     $scope.auth = $cookies.get('auth');
-    $scope.toggle = false; 
+    $scope.toggle = false;
+    $scope.messages = []; 
+
+    $scope.message = {
+      userid: $cookies.get('userid'),
+      text: ''
+    };
     $scope.newPost = {
       userid: $cookies.get('userid'),
       title: '',
@@ -54,6 +60,29 @@ appModule.component('sidebarComponent', {
         .closeTo(angular.element(document.querySelector('#right')))
       );
     };
+    $scope.message = {
+      userid: $cookies.get('userid'),
+      text: ''
+    };
+    
+    socket.on('connection', function(data) {
+      $scope.$apply(function () {
+        $scope.messages = data;
+      });
+    });
+
+    $scope.sendMessage = function(keyEvent) {
+      if (keyEvent.which === 13) {
+        socket.emit('msg', $scope.message);
+        $scope.message.text = '';
+      }
+    };
+
+    socket.on('newmsg', function(data) {
+      $scope.$apply(function () {
+        $scope.messages = data;
+      });
+    });
 
   }
 });
