320703ea835f4ab17434417915aba5b1b30434be
impersin <closer124@gmail.com>
2017-07-24 15:14:00 -0700

    Implemented feature that user can upload and change profile image

--- client/app/components/profile.component.js
+++ client/app/components/profile.component.js
@@ -2,12 +2,16 @@ appModule.component('profileComponent', {
   templateUrl: 'app/templates/profile.html',
   controllerAs: 'profileController',
   controller: function($scope, $cookies, Factory) {
+    $scope.user = $cookies.get('userid');
+    $scope.auth = $cookies.get('auth');
     $scope.title = 'about page';
     $scope.fieldset = false;
     $scope.title = 'FIFA 2018 Match Result Page';
     $scope.fieldset = false;
     $scope.win = $cookies.get('win');
     $scope.lose = $cookies.get('lose');
+    $scope.profileForm = false;
+    $scope.profileImage = 'https://s3-us-west-1.amazonaws.com/fifatalk/' + $cookies.get('profileImage');
     $scope.matchInfo = {
       playerName: '',
       location: '',
@@ -33,16 +37,7 @@ appModule.component('profileComponent', {
       brazil: 'br'
     };
 
-    $scope.toggleFieldset = function() {
-      $scope.fieldset = !$scope.fieldset;
-    };
-
-    $scope.toggleFieldset = function() {
-      $scope.fieldset = !$scope.fieldset;
-    };
-
-    Factory.getAll().then(function(res) {
-    
+    Factory.getAll().then(function(res) {    
       $scope.userName = $cookies.get('userName');
       $scope.matchResults = res.data.filter(function(match) {
         if (match['Player Name'].toUpperCase() === $scope.userName.toUpperCase()) {
@@ -50,5 +45,22 @@ appModule.component('profileComponent', {
         } 
       });
     });
+
+    $scope.formToggle = function() {
+      $scope.profileForm = !$scope.profileForm;
+    };
+
+    $scope.sendProfileImage = function() {
+      var file = $scope.file;
+      var fd = new FormData();
+      fd.append('file', file);
+      Factory.uploadFile(fd).then(function(res) {
+        Factory.addProfileImage({userid: $scope.user, fileName: file.name}).then(function(res) {
+          $scope.profileImage = 'https://s3-us-west-1.amazonaws.com/fifatalk/' + res.data;
+          $scope.profileForm = false;
+        });
+      });
+    };
+
   }
 });
