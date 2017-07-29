a5f1e5a53f8796dd6a1444a9875d3c0c5fade030
impersin <closer124@gmail.com>
2017-07-29 13:16:29 -0700

    Added url for mobile version not to autoplay

--- client/app/components/landing.component.js
+++ client/app/components/landing.component.js
@@ -2,7 +2,8 @@ appModule.component('landingComponent', {
   templateUrl: 'app/templates/landing.html',
   controllerAs: 'landingController',
   controller: function($scope, $http) {
-    $scope.videoUrl = 'https://www.youtube.com/watch?v=QV7PK8AVEKA?autoplay=1';
+    $scope.videoUrl = 'https://www.youtube.com/watch?v=QV7PK8AVEKA?';
+    $scope.mobileVideoUrl = 'https://www.youtube.com/watch?v=QV7PK8AVEKA?';
     $scope.title = 'https://www.youtube.com/watch?v=QV7PK8AVEKA?autoplay=1';
     
     $scope.sendRequest = function() {
