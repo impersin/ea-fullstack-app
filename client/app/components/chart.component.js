3c9c1e6943841b8d32a47469122ae065671b93c6
impersin <closer124@gmail.com>
2017-08-05 15:30:19 -0700

    Invoked getAllPlayer to initialize player data

--- client/app/components/chart.component.js
+++ client/app/components/chart.component.js
@@ -11,17 +11,14 @@ appModule.config(['ChartJsProvider', function (ChartJsProvider) {
 }]).component('chartComponent', {
   templateUrl: 'app/templates/chart.html',
   controllerAs: 'chartController',
-  controller: function($scope, $http, $location, $timeout) {
+  controller: function($scope, $http, $location, $timeout, Factory) {
     $scope.toggle = true;
+    Factory.getAllPlayer().then(function(res) {
+      $scope.players = res.data;
+    });
+
     $scope.labels = ['Pace', 'Dribbling', 'Shooting', 'Defending', 'Passing', 'Physicality'];
     // $scope.series = ['Series A', 'Series B'];
-    $scope.data = [
-    [98, 98, 99, 50, 92, 94]
-    ];
-    $scope.onClick = function (points, evt) {
-      console.log(points, evt);
-    };
-    
   }
 });
 
