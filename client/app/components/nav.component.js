f1d0f3e366f5d3c867629197e28461541139a404
impersin <closer124@gmail.com>
2017-08-06 17:51:50 -0700

    Implemented hadScrolled to hide navbar when user scroll

--- client/app/components/nav.component.js
+++ client/app/components/nav.component.js
@@ -155,6 +155,44 @@ appModule.component('navComponent', {
         
       }
     };
+
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $('.navbar').outerHeight();
+
+    $(window).scroll(function(event) {
+      // console.log('scrolling');
+      didScroll = true;
+    });
+
+    var hasScrolled = function() {
+      var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+      if (Math.abs(lastScrollTop - st) <= delta) { return; }
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+      if (st > lastScrollTop && st > navbarHeight) {
+        // Scroll Down
+        $('.navbar').removeClass('nav-down').addClass('nav-up');
+      } else {
+        // Scroll Up
+        if (st + $(window).height() < $(document).height()) {
+          $('.navbar').removeClass('nav-up').addClass('nav-down');
+        }
+      }
+      lastScrollTop = st;
+    };
+
+    setInterval(function() {
+      if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+      }
+    }, 100);
+
   }
 });
 
